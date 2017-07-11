8cdac90dfd6c67221ecf1277638bb3a229325b31
Simon Fugere <smonyk@gmail.com>
2017-07-11 17:32:10 -0400

    removed old files

--- resources/js/controllers/homeCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('pocketBetaApp')
-.controller('homeCtrl', function ($scope) {
-	'use strict';
-	$scope.soon = 'Soon';
-});
--- resources/js/controllers/masterCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('pocketBetaApp')
-.controller('masterCtrl', function ($scope) {
-	'use strict';
-	$scope.soon = "Soon";
-});
